440cfe326346f8e6915e83b30b8fb2e99d8a50da
Anwar Sarmiento <anwarsarmiento@gmail.com>
2020-09-30 21:47:48 -0600

    Feat: iniciando el proyecto para realización de proformas

---
+++ resources/js/Jetstream/ApplicationLogo.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+      <h1>Realice una Proforma</h1>
+    </div>
+</template>
---
+++ resources/js/Jetstream/Welcome.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+            <div>
+                <jet-application-logo class="block h-12 w-auto" />
+            </div>
+
+            <div class="mt-8 text-2xl">dfsfd
+                <form @submit.prevent="submit">
+                    <label for="first_name">First name:</label>
+                    <input id="first_name" v-model="form.first_name" />
+                    <label for="last_name">Last name:</label>
+                    <input id="last_name" v-model="form.last_name" />
+                    <label for="email">Email:</label>
+                    <input id="email" v-model="form.email" />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+
+
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+    import JetApplicationLogo from './../Jetstream/ApplicationLogo'
+
+    export default {
+        components: {
+            JetApplicationLogo,
+        },
+        data() {
+            return {
+                form: {
+                    first_name: null,
+                    last_name: null,
+                    email: null,
+                },
+            }
+            }, methods: {
+            submit() {
+                this.$inertia.post('/users', this.form)
+            },
+        },
+    }
+</script>
